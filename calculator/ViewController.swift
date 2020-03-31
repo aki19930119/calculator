93f99c4d28144b6bef9c843e8c9a7c3162ed5610
aki0913 <celcior0913@gmail.com>
2020-03-31 22:13:34 +0900

    first commit

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var calcularCollectionView: UICollectionView!
+    @IBOutlet weak var calculatorHeightConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
